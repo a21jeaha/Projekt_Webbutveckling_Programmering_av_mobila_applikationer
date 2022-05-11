e1956e1337379a7a39b216c983b82c8482cc6ea2
Jean-Paul <a21jeaha@student.his.se>
2022-05-11 14:48:16 +0200

    classes Penguin and Auxdata created.
    getters and setters implemented

---
+++ app/src/main/java/com/example/myapplication/Auxdata.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+public class Auxdata {
+
+    private String info;
+    private String img;
+
+    public Auxdata(String info, String img) {
+        this.info = info;
+        this.img = img;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Penguin.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+public class Penguin {
+    private String name;
+    private String eats;
+    private String size;
+    private Auxdata auxdata;
+
+    public Penguin(String name, String eats, String size, Auxdata auxdata) {
+        this.name = name;
+        this.eats = eats;
+        this.size = size;
+        this.auxdata = auxdata;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEats() {
+        return eats;
+    }
+
+    public void setEats(String eats) {
+        this.eats = eats;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Auxdata getAuxdata() {
+        return auxdata;
+    }
+
+    public void setAuxdata(Auxdata auxdata) {
+        this.auxdata = auxdata;
+    }
+}
+
